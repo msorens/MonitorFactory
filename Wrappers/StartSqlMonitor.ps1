42ee307f8c3a90e841a5dd92428ac954ba22ce2f
msorens <msorens@users.noreply.github.com>
2015-11-14 20:09:13 -0800

    Enhancement: allow option to specify literal query rather than query from file

--- Wrappers/StartSqlMonitor.ps1
+++ Wrappers/StartSqlMonitor.ps1
@@ -1,25 +1,27 @@
 # Wrapper for Start-Monitor to execute SQL code from a file
 
 function Start-SqlMonitor (
-    [string]$path,
-    [string]$server,
-    [string]$database,
+    [Parameter(ParameterSetName='FileQuery', Mandatory)][string]$path,
+    [Parameter(ParameterSetName='InlineQuery', Mandatory)][string]$query,
+    [Parameter(Mandatory)][string]$server,
+    [Parameter(Mandatory)][string]$database,
     [string]$title = '',
     [string]$interval = '5sec'
 )
 {
+    if ($path) { $query = Get-Content $path }
     $monitorParams = @{
         AsJob = $true
         Interval = $interval
         ScriptBlock = {
-            $results = Invoke-Sqlcmd -InputFile $args[0] -Server $args[1] -Database $args[2]
-			if ($results) {
-				# Filters out meta-columns added by PowerShell bug
-				$names = ($results[0] | Get-Member -Type property).Name
-				$results | Select-Object $names
-			}
+            $results = Invoke-Sqlcmd -Query $args[0] -Server $args[1] -Database $args[2]
+            if ($results) {
+                # Filters out meta-columns added by PowerShell bug
+                $names = ($results[0] | Get-Member -Type property).Name
+                $results | Select-Object $names
+            }
         }
-        ArgumentList = $path, $server, $database
+        ArgumentList = $query, $server, $database
     }
     if ($title) { $monitorParams.DisplayName = $title }
     Start-Monitor @monitorParams
